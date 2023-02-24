2cbc7f05bc7029090fe900c37cbf6d481e10b6de
Ryan Valverde de Ward <ryandward@gmail.com>
2023-02-24 12:11:31 -0600

    add: made very substantial changes in logic to finding difference between genomes

--- Plotters/plotter_for_volcanos.R
+++ Plotters/plotter_for_volcanos.R
@@ -3,9 +3,9 @@ library(hrbrthemes)
 
 doc_theme <- theme_ipsum(base_family = "Arial", caption_margin = 12, axis_title_size = 12, axis_col = "black")
 
-
 volcano_plot <- function(median_melted_results, conditions, max_left = 5, max_right = 5, max_top = 5, max_sig = 1e-75){
 	median_melted_results %>% 
+		mutate(locus_tag = gsub("ZMO1_", "", locus_tag)) %>%
 		mutate(gene_name = case_when(
 			gene_name == "." | is.na(gene_name)  ~ locus_tag,
 			gene_name != "." ~ gene_name)) %>%
@@ -49,14 +49,16 @@ volcano_plot <- function(median_melted_results, conditions, max_left = 5, max_ri
 				FDR < 0.05 & 
 					(medLFC_ix <= max_left & medLFC < -0.5 | 
 					 	(medLFC_desc_ix <= max_right & medLFC > 0.5 ) | 
-					 	FDR_ix <= max_top)),
+					 	FDR_ix <= max_top) |
+					gene_name %in% c("recJ", "oxyR", "sod", "fpr", "fdxA") |
+					gene_name %like% "hem"),
 			aes(label = gene_name),
 			force = 7.5,
 			segment.size = 0.15,
 			min.segment.length = 0,
 			box.padding = 2,
 			point.padding = .25,
-			size = 2,
+			size = 2.5,
 			max.overlaps = Inf,
 			colour = "black") +
 		facet_wrap(~ condition, scales = "free") +
@@ -64,28 +66,7 @@ volcano_plot <- function(median_melted_results, conditions, max_left = 5, max_ri
 			values = c(
 				"Significant" = 0.75,
 				"Not Significant" = 0.15))
-	# +
-	# 	scale_fill_manual(
-	# 		values = c(
-	# 			"Other" = "grey",
-	# 			"Ribosome" = "#E31A1C",
-	# 			"Ribosome+Other" = "#FB9A99",
-	# 			"Ox Phos" = "#6A3D9A",
-	# 			"Ox Phos+Other" = "#CAB2D6",
-	# 			"LOS" = "#33A02C",
-	# 			"LOS+Other" = "#B2DF8A",
-	# 			"Cell Wall/PG" = "#FF7F00",
-	# 			"Cell Wall/PG+Other" = "#FDBF6F",
-	# 			"tRNA Ligase" = "#1F78B4",
-	# 			"tRNA Ligase+Other" = "#A6CEE3")) +
-	# 	guides(fill = guide_legend(
-	# 		override.aes = list(shape = 21, size = 5))) + 
-	# 	guides(alpha = guide_legend(
-	# 		override.aes = list(shape = 21, size = 5, fill = "black"))) +
-	# 	scale_alpha_manual(
-	# 		values = c(
-	# 			"Significant" = 0.75,
-	# 			"Not Significant" = 0.15)) +
-	# 	scale_size_area(breaks = c(1, 5, 10, 13))
+
 }
 
+volcano_plot(median_melted_results, c("aerobic_T1 - anaerobic_T1", "aerobic_T2 - anaerobic_T2", "aerobic_T3 - anaerobic_T3"), 10, 10, 2)
--- ortho_cleanup.R
+++ ortho_cleanup.R
@@ -56,18 +56,17 @@ genome_order <- c(model_organisms, sort(setdiff(levels(orthos$genome), model_org
 
 orthos$genome <- factor(orthos$genome, levels = genome_order)
 
-
 orthos_summary <- orthos %>% 
 	group_by(HOG, OG, `Gene Tree Parent Clade`, genus, genome) %>% 
 	summarise(N = n()) 
 
-orthos_wider <- orthos_summary %>% 
+orthos_table <- orthos_summary %>% 
 	data.table %>% 
 	data.table::dcast(genome + genus ~ HOG, value.var = "N", fill = 0) %>%
 	as_tibble()
 
-alpha_ecoli_orthos_wider <- 	
-	orthos_wider %>% 
+alpha_ecoli_orthos_table <- 	
+	orthos_table %>% 
 	filter(
 		genome == "Escherichia coli BW25113" |
 		genus %in% c(
@@ -81,29 +80,186 @@ alpha_ecoli_orthos_wider <-
 		"Rhodopseudomonas",
 		"Novosphingobium"))
 
-alpha_ecoli_orthos_wider <- alpha_ecoli_orthos_wider %>% 
+alpha_ecoli_orthos_table <- alpha_ecoli_orthos_table %>% 
 	data.table %>% 
 	melt(id.vars = c("genome", "genus"), variable.name = "HOG", value.name = "N") %>% 
 	filter(N > 0) %>% 
 	dcast(genome + genus ~ HOG, value.var = 'N', fill = 0) %>% 
 	as_tibble()
 
-d <- 
-	alpha_ecoli_orthos_wider %>%
-	select(-genome, -genus) %>%
-	dist(method = "canberra")
+# d <- 
+# 	alpha_ecoli_orthos_table %>%
+# 	select(-genome, -genus) %>%
+# 	dist(method = "canberra")
+# 
+# d_fit <- cmdscale(d, eig = TRUE, k = 2)
+# 
+# d_scale <- d_fit$points %>% data.table
+# 
+# d_scale$genome <- alpha_ecoli_orthos_table$genome
+# d_scale$genus <- alpha_ecoli_orthos_table$genus
+# 
+# d_scale %>% ggplot(aes(x = V1, y = V2)) + 
+# 	geom_point(aes(fill = genus), size = 5, shape = 21, alpha = 0.5) +
+# 	geom_text_repel(aes(label = genome)) +
+# 	doc_theme
 
-d_fit <- cmdscale(d, eig = TRUE, k = 2)
+##########################################################################################
 
-d_scale <- d_fit$points %>% data.table
+p_load(data.table, viridis, grid, tidyverse)
+p_load_current_gh("jokergoo/ComplexHeatmap")
 
-d_scale$genome <- alpha_ecoli_orthos_wider$genome
-d_scale$genus <- alpha_ecoli_orthos_wider$genus
+ecoli_genes <- fread("Escherichia_coli_BW25113.tsv")
 
-d_scale %>% ggplot(aes(x = V1, y = V2)) + 
-	geom_point(aes(fill = genus), size = 5, shape = 21, alpha = 0.5) +
-	geom_text_repel(aes(label = genome)) +
-	doc_theme
+alpha_ecoli_presence <- alpha_ecoli_orthos_table %>% 
+	data.table %>% 
+	melt(id.vars = c("genus", "genome"), variable.name = "HOG", value.name = "N") %>% 
+	mutate(present_orthogroup = case_when(N > 0 ~ TRUE, N == 0 ~ FALSE)) %>% 
+	dcast(HOG ~ genome, value.var = "present_orthogroup")
+
+alpha_ecoli_presence_combomat <- alpha_ecoli_presence %>% make_comb_mat
+
+##########################################################################################
+# This function takes a list of genome names and returns a concatenated string 
+# representing the presence or absence of each genome in the list. The string is 
+# constructed by checking each genome in the input list against a reference 
+# list of all genomes, and setting a "1" if the genome is present, or "0" if it 
+# is absent. Reference list is obtained from the matrix: "alpha_ecoli_presence_combomat"
+
+genome_presence <- function(genomes, all_genomes) {
+	presence_vector <- rep(0, length(all_genomes))
+	for (genome in genomes) {
+		idx <- match(genome, all_genomes)
+		if (!is.na(idx)) {
+			presence_vector[idx] <- 1
+		}
+	}
+	return(paste(presence_vector, collapse = ""))
+}
+
+idx_to_hog <- function(ortho_table, idx, combo_matrix) {
+	return(alpha_ecoli_presence[extract_comb(alpha_ecoli_presence_combomat, idx), ] %>% pull(HOG))
+}
+
+# genome_presence("Zymomonas mobilis", alpha_ecoli_presence_combomat %>% rownames)
+# genome_presence(c("Zymomonas mobilis", "Escherichia coli BW25113"), alpha_ecoli_presence_combomat %>% rownames)
+
+all_genomes <- alpha_ecoli_presence_combomat %>% rownames
+
+##########################################################################################
+
+#All Alphas + E. coli: https://version-11-5.string-db.org/cgi/network?networkId=bjUeidx5OYaE
+all_present <- genome_presence(all_genomes, all_genomes)
+
+all_present <- alpha_ecoli_presence[extract_comb(
+	alpha_ecoli_presence_combomat, all_present_idx), ]
+
+#All Alphas - E. coli: https://version-11-5.string-db.org/cgi/network?networkId=bzNJgxu0NyeQ 
+all_alphas_only_idx <- genome_presence(all_genomes[!(all_genomes %in% ("Escherichia coli BW25113"))], all_genomes)
+
+all_alphas_only <- alpha_ecoli_presence[extract_comb(
+	alpha_ecoli_presence_combomat, all_alphas_only_idx), ]
+
+#Zymomonas mobilis only - https://version-11-5.string-db.org/cgi/network?networkId=bDpAcDlAAZj
+Zm_only <- alpha_ecoli_presence[extract_comb(
+	alpha_ecoli_presence_combomat, genome_presence("Zymomonas mobilis", all_genomes)), ]
+
+##########################################################################################
+# create a table with info about each genome
+
+genome_sets <- data.table()
+for (i in all_genomes) {
+	
+	genome_sets <- orthos %>% 
+		filter(genome == i) %>% 
+		select(genome, genus, locus_tag) %>% 
+		mutate(set = "All genes") %>%
+		data.table %>% rbind(genome_sets)
+
+	genome_sets <- all_present %>% 
+		select(HOG) %>% 
+		inner_join(orthos %>% filter(genome == i), multiple = "all") %>% 
+		select(genome, genus, locus_tag) %>% 
+		mutate(set = "Present in all alphas and E. coli") %>% 
+		data.table %>% rbind(genome_sets)
+	
+	genome_sets <- all_alphas_only %>% 
+		select(HOG) %>% 
+		inner_join(orthos %>% filter(genome == i), multiple = "all") %>% 
+		select(genome, genus, locus_tag) %>% 
+		mutate(set = "Common only to all alphas") %>% 
+		data.table %>% rbind(genome_sets)
+	
+	genome_sets <-	alpha_ecoli_presence[extract_comb(
+		alpha_ecoli_presence_combomat, 
+		genome_presence(i, all_genomes)), ] %>% 		
+		select(HOG) %>%
+		inner_join(orthos %>% filter(genome == i), multiple = "all") %>% 
+		select(genome, genus, locus_tag) %>%
+		mutate(set = "Unique to genome") %>% 
+		data.table %>% rbind(genome_sets)
+}
+
+genome_sets$genome <- factor(genome_sets$genome, levels = genome_order)
+
+genome_sets_stats <- genome_sets %>% 
+	group_by(genome, set) %>% 
+	summarise(size = n()) %>% 
+	data.table %>% 
+	dcast(genome ~ set, value.var = "size", fill = 0) %>%
+	melt(id.vars = "genome", value.name = "size", variable.name = "set")
+
+genome_sets_stats$genome <- factor(genome_sets_stats$genome, levels = genome_order)
+
+##########################################################################################
+# take info from upset plot, but make it simpler and plot
+
+palette <- brewer.pal(8, "Paired")
+my_colors <- palette[c(2, 4, 6, 8)]
+
+genome_sets_stats_plot <- genome_sets_stats %>% 
+	ggplot(aes(fill = set, y = `size`, x = genome)) + 
+	geom_bar(position = "dodge", stat = "identity") +
+	doc_theme +
+	theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+	scale_fill_manual(values = my_colors)
+
+print(genome_sets_stats_plot)
+
+##########################################################################################
+# Hard questions
 
 
+
+# Now we don't care about E. coli. These HOGS are present everywhere in Alphas and may be in E. coli
+## Find index
+alphas_p_ec_idx <- genome_presence(all_genomes, all_genomes)
+alphas_m_ec_idx <- genome_presence(all_genomes[!(all_genomes %in% (c("Escherichia coli BW25113")))], all_genomes)
+
+# Find HOGs pom (plus or minus) E. coli
+alphas_pom_ec <- c(
+	idx_to_hog(alpha_ecoli_presence, alphas_p_ec_idx, alpha_ecoli_presence_combomat), 
+	idx_to_hog(alpha_ecoli_presence, alphas_m_ec_idx, alpha_ecoli_presence_combomat)) %>% 
+	droplevels %>% data.table(HOG = .) %>%
+	arrange(HOG)
+
+# Now we don't care about E. coli or Zymomonas
+## Find index
+alphas_m_zm_idx <- genome_presence(all_genomes[!(all_genomes %in% (c("Zymomonas mobilis")))], all_genomes)
+alphas_m_ec_m_ec_idx <- genome_presence(all_genomes[!(all_genomes %in% (c("Escherichia coli BW25113", "Zymomonas mobilis")))], all_genomes)
+
+# Find HOGs pom (plus or minus) E. coli, Zymomonas
+alphas_pom_ec_pom_zm <- c(
+	idx_to_hog(alpha_ecoli_presence, alphas_p_ec_idx, alpha_ecoli_presence_combomat),
+	idx_to_hog(alpha_ecoli_presence, alphas_m_ec_idx, alpha_ecoli_presence_combomat),
+	idx_to_hog(alpha_ecoli_presence, alphas_m_zm_idx, alpha_ecoli_presence_combomat), 
+	idx_to_hog(alpha_ecoli_presence, alphas_m_ec_m_ec_idx, alpha_ecoli_presence_combomat)) %>% 
+	droplevels %>% data.table(HOG = .) %>%
+	arrange(HOG)
+
+# Find the difference and map it onto Caulobacter 
+alphas_pom_ec_m_zm_mapped_cc <- alphas_pom_ec_pom_zm %>% 
+	anti_join(alphas_pom_ec) %>% 
+	inner_join(alpha_ecoli_presence) %>% 
+	inner_join(orthos %>% filter(genome == "Caulobacter crescentus"), multiple = "all") %>% select(locus_tag)
 

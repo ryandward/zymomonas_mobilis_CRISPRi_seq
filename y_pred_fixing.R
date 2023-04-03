48006d5fea4c05f6c7e5721d95b26a017091da7d
Ryan Valverde de Ward <ryandward@gmail.com>
2023-04-03 11:39:22 -0500

    add: diagnostics for y_preds

--- y_pred_fixing.R
+++ y_pred_fixing.R
@@ -14,3 +14,19 @@ results_perfect <- melted_results2 %>% filter(type == "perfect")
 results_mismatch <- results_mismatch %>% select(-type) %>% rename(LFC_mismatch = LFC, FDR_mismatch = FDR)
 results_perfect <- results_perfect %>% select(-spacer, -y_pred_calc, -type) %>% rename(LFC_perfect = LFC, FDR_perfect = FDR)
 
+# Randomly select a locus_tag
+random_locus_tag <- sample(unique(results_mismatch$locus_tag), 1)
+
+# Filter results_mismatch based on the random_locus_tag and plot using ggplot2
+results_mismatch %>% 
+	filter(locus_tag == random_locus_tag) %>% 
+	ggplot(aes(x = y_pred_calc, y = LFC_mismatch)) + 
+	facet_wrap(~condition) + 
+	geom_line() + ggtitle(random_locus_tag) + geom_point()
+
+# Draw galaxy plots
+results_perfect %>%
+	inner_join(results_mismatch, by = join_by(original, locus_tag, gene_name, condition)) %>%
+	ggplot(aes(x = LFC_perfect, y = LFC_mismatch, color = y_pred_calc)) +
+	geom_point(size = 0.5, alpha = 0.5) +
+	scale_color_viridis(option = "C") + facet_wrap(~condition)
